142c7a4def6e3680f76dfaaeef935cc1bca89219
pixeljam <MT156314@hrsfc.ac.uk>
2024-09-18 00:50:31 +0100

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Milo Tek on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
